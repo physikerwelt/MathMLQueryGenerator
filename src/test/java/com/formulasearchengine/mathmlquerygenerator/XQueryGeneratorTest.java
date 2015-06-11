ac6f3ffee58374ca61c383d4ae1cab81a31f2cda
physikerwelt <wiki@physikerwelt.de>
2015-06-11 18:30:07 +0200

    Make qVars accessible from outside xQueryGenerator
    Close #10

--- src/test/java/com/formulasearchengine/mathmlquerygenerator/XQueryGeneratorTest.java
+++ src/test/java/com/formulasearchengine/mathmlquerygenerator/XQueryGeneratorTest.java
@@ -8,6 +8,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Map;
 import java.util.Scanner;
 
 public class XQueryGeneratorTest extends TestCase {
@@ -125,4 +127,18 @@ public class XQueryGeneratorTest extends TestCase {
 		assertNull( "Input without math should return null", qg.toString() );
 	}
 
+	public void testqVarGetter() throws Exception {
+		final String expectedVariableName = "x";
+		final String firstExpectedLocation = "/*[2]/*[2]";
+		final String testInput = getFileContents( "com/formulasearchengine/mathmlquerygenerator/mws/qqx2x.xml" );
+		Document query = XMLHelper.String2Doc( testInput );
+		XQueryGenerator xQueryGenerator = new XQueryGenerator( query );
+		Map<String, ArrayList<String>> qVars = xQueryGenerator.getQvar();
+		assertEquals( 1, qVars.entrySet().size() );
+		Map.Entry<String, ArrayList<String>> firstEntry = qVars.entrySet().iterator().next();
+		assertEquals( expectedVariableName, firstEntry.getKey() );
+		ArrayList<String> xPaths = firstEntry.getValue();
+		assertEquals( 2, xPaths.size() );
+		assertEquals( firstExpectedLocation, xPaths.get(0) );
+	}
 }
